4cffd8726249ad0be6f0924d5e9d3b8b2bff8ec7
Raphael Sofaer <rafi@sofaer.net>
2014-02-03 12:25:18 -0800

    Working with devise in the system

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Submissions::Application.config.secret_key_base = 'b467b3440439d5ec4468a18ff0ce5a78ba77aae7e6f5b52a210cd5284c509c43f2f2ffd2860b00d9dd8ee416eea5780bd2a3929a96f85f841b8fcbd357a321ce'
+Submissions::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
