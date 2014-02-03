d6821df2e4e32678af62fdf5acd135d9bc98a24e
Raphael Sofaer <rafi@sofaer.net>
2014-02-03 11:32:15 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WDI January 2014 Submissions management
+
+An app for managing student lab submissions for the January cohort of General Assembly WDI in San Francisco.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Submissions::Application.config.secret_key_base = 'b467b3440439d5ec4468a18ff0ce5a78ba77aae7e6f5b52a210cd5284c509c43f2f2ffd2860b00d9dd8ee416eea5780bd2a3929a96f85f841b8fcbd357a321ce'
