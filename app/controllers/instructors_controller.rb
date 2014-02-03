059465c59c28d567e95bd01ab661d26103c615fb
Raphael Sofaer <rafi@sofaer.net>
2014-02-03 14:17:03 -0800

    Add SiteController, link to the gh authorize path.

--- app/controllers/instructors_controller.rb
+++
@@ -1,4 +0,0 @@
-class InstructorsController < ApplicationController
-  def home
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Submissions::Application.routes.draw do
   devise_for :users
-  root to: 'instructors#home'
+  root to: 'site#index'
 end
