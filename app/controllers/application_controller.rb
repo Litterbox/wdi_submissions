ce7075a4f5ca973fe93a5e42862d18554b66b658
Alex Notov <anotov@gmail.com>
2014-02-15 19:26:28 -0800

    Remove debugging from production code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(res)
-    binding.pry
   	stored_location_for(res) ||
 	    if res.is_a? Instructor
 	      instructors_students_path
