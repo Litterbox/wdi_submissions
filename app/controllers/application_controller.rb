98ba54dce2a568e867cf0bfb09237a03762cb5a2
Alex Notov <anotov@gmail.com>
2014-02-19 12:37:38 -0800

    Fixing some whitespace issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(res)
-  	stored_location_for(res) ||
-	    if res.is_a? Instructor
+    stored_location_for(res) ||
+      if res.is_a? Instructor
         students_path
-	    else
-	      student_path(res)
-	    end
+      else
+        student_path(res)
+      end
   end
-    
 end
