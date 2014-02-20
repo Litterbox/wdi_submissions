f22bc0f70313173f0dc9eefd2529a13831952efb
Alex Notov <anotov@gmail.com>
2014-02-19 16:02:21 -0800

    Abstract out the omniauth hash creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,33 +2,23 @@ require 'spec_helper'
 
 describe User do
   describe '.find_for_gh_oauth' do
-    before do
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
-        provider: 'github',
-        uid: '123545',
-        info: {
-          name: 'github user',
-          image: 'gravatar image',
-          nickname: 'gh-nickname'
-        } 
-      })
-      @auth_hash = OmniAuth.config.mock_auth[:github] 
-    end
     context 'user exists' do
       it 'should retun the user' do
-        @user = FactoryGirl.create(:student, uid: 123545)
-        User.find_for_gh_oauth(@auth_hash).should == @user
+        @user = FactoryGirl.create(:student)
+        User.find_for_gh_oauth(mock_auth_hash(@user)).should == @user
       end
     end
     context 'user does NOT exist' do
       it 'should return a STUDENT' do
-        User.find_for_gh_oauth(@auth_hash).type.should == 'Student'
+        User.find_for_gh_oauth(mock_auth_hash).type.should == 'Student'
       end
     end
     context 'when INSTRUCTOR matching gh handle IS FOUND' do
       it 'should return the instructor record' do
-        @instructor = FactoryGirl.create(:instructor, uid: 123545)
-        User.find_for_gh_oauth(@auth_hash).id.should ==  @instructor.id
+        @instructor = FactoryGirl.create(:instructor)
+        User.find_for_gh_oauth(
+          mock_auth_hash(@instructor)
+        ).id.should ==  @instructor.id
       end
     end
   end
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -3,27 +3,31 @@
 
 # This module authenticates users for request specs.#
 module ValidUserRequestHelper
+  def mock_auth_hash user_object=FactoryGirl.build(:user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: user_object.provider,
+      uid: user_object.uid,
+      info: {
+        name: user_object.name,
+        image: 'gravatar image',
+        nickname: user_object.gh_nickname
+      } 
+    })
+  end
+
   # Define a method which signs in as a valid user.
   def sign_in_as_a_valid_instructor
     @instructor = FactoryGirl.create(:instructor) 
 
-    OmniAuth.config.mock_auth[:github] = {
-      provider: @instructor.provider,
-      uid: @instructor.uid,
-      info: {
-        name: @instructor.name,
-        image: 'gravatar image',
-        nickname: @instructor.gh_nickname 
-      } 
-    }
+    mock_auth_hash @instructor
 
     get user_omniauth_callback_path(:github), format: :json
   end
 end
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
-  config.include ValidUserRequestHelper, :type => :request 
+  config.include Devise::TestHelpers, type: :controller
+  config.include ValidUserRequestHelper
 end
 
 OmniAuth.config.test_mode = true
