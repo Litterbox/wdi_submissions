f16e21dc57c4a2211ca1e3ee26b227429d26f7a0
Alex Notov <anotov@gmail.com>
2014-02-20 15:49:09 -0800

    Now displaying all pull requests for all wdi-sf-jan-2014 org repos

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < BaseController
   def submissions
     @submissions = Octokit.org_repos(ENV['GA_ORG_NAME']).map do |repo|
-      Octokit.pull_requests(repo.name)
+      Octokit.pull_requests("#{ENV['GA_ORG_NAME']}/#{repo.name}")
     end.flatten
   end
 end
