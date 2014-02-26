de4e6419d4fe1ddd6046a1a96df0f5c34cd9dba8
Raphael Sofaer <rafi@sofaer.net>
2014-02-26 11:05:33 -0800

    Add a warning when GA_ORG_NAME is not set.

--- app/services/octokit_wrapper.rb
+++ app/services/octokit_wrapper.rb
@@ -3,6 +3,10 @@ class OctokitWrapper
   include Singleton
 
   def client
+    if ENV['GA_ORG_NAME'].nil? && @client.nil?
+      Rails.logger.warn("Please set ENV['GA_ORG_NAME']
+       to the github organization which contains your assignments.")
+    end
     @client ||= Octokit::Client.new(
       client_id: ENV['GITHUB_APP_ID'],
       client_secret: ENV['GITHUB_APP_SECRET']
